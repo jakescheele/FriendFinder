3d7777956700053a900424dcef7214569f4bceb2
Jake S <45572826+jakescheele@users.noreply.github.com>
2019-03-04 21:20:01 -0600

    First push to begin Heroku testing

---
+++ server.js
@@ -0,0 +1,27 @@
+// ======================================
+// Require dependencies
+require('dotenv').config()
+var express = require('express')
+
+// Initialiaze express
+var app = express();
+// Set up Port
+var PORT = process.env.PORT
+// Set up express data parsing
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+// ======================================
+
+// Routes
+
+require('./app/routing/htmlRoutes')(app);
+require('./app/routing/apiRoutes')(app);
+
+// ======================================
+
+// Listener
+
+app.listen(PORT, function(){
+    console.log('App listening on port: ' + PORT)
+})
